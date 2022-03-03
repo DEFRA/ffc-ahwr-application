a4fe25a14664800a82f0e252e1912fccf189cbc6
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2022-03-03 17:08:41 +0000

    Update some logging

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const init = async () => {
 
   const db = require('./data')
   const applications = await db.models.application.findAll()
-  console.log(`There are ${applications.length} applications`)
+  console.log(`There are ${applications.length} applications in the database`)
   applications.forEach(a => console.log(`Application ${a.applicationId} with reference ${a.reference}`))
 }
 
