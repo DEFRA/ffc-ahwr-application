b169f48ac0f876b11af3e91a2183d027e77e728d
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2022-03-08 10:06:23 +0000

    Simplify

--- app/data/models/application.js
+++ app/data/models/application.js
@@ -1,12 +1,9 @@
-module.exports = (sequelize, DataTypes) => {
-  const application = sequelize.define('application', {
-    applicationId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    reference: DataTypes.STRING
-  },
-  {
-    tableName: 'application',
-    freezeTableName: true,
-    timestamps: false
-  })
-  return application
-}
+module.exports = (sequelize, DataTypes) => sequelize.define('application', {
+  applicationId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  reference: DataTypes.STRING
+},
+{
+  tableName: 'application',
+  freezeTableName: true,
+  timestamps: false
+})
