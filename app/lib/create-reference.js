2575e218e77c32881740361714e091e172e93dfa
Shivanshu Patel <shivanshuit914@gmail.com>
2022-03-23 15:58:51 +0000

    chore: refactor create reference code

--- app/lib/create-reference.js
+++ app/lib/create-reference.js
@@ -1,5 +1,10 @@
 const { v4: uuid } = require('uuid')
 
+/**
+ * Generate unique reference number
+ * ex. VV22-B471-F25C
+ * @returns string
+ */
 module.exports = () => {
   const id = uuid().split('-').shift().toLocaleUpperCase('en-GB').match(/.{1,4}/g).join('-')
   const reference = `VV${new Date().getFullYear().toString().slice(-2)}-${id}`
