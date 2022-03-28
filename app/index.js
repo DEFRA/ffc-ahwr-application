66f45f07d850ffd7c8bd48f978545d2c98b8bc15
Shivanshu Patel <shivanshuit914@gmail.com>
2022-03-28 19:14:06 +0100

    feat: remove unnecessary code

--- app/index.js
+++ app/index.js
@@ -5,11 +5,6 @@ const init = async () => {
   await messageService.start()
   await server.start()
   console.log('Server running on %s', server.info.uri)
-
-  const db = require('./data')
-  const applications = await db.models.application.findAll()
-  console.log(`There are ${applications.length} applications`)
-  applications.forEach(a => console.log(`Application ${a.id} with reference ${a.reference}`))
 }
 
 process.on('unhandledRejection', async (err) => {
