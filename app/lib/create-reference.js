89c4abc3be6139c466c76e327f34e93b754c9b26
Shivanshu Patel <shivanshuit914@gmail.com>
2022-03-29 11:28:02 +0100

    feat: remove year from reference numbr

--- app/lib/create-reference.js
+++ app/lib/create-reference.js
@@ -7,5 +7,5 @@ const { v4: uuid } = require('uuid')
  */
 module.exports = () => {
   const id = uuid().split('-').shift().toLocaleUpperCase('en-GB').match(/.{1,4}/g).join('-')
-  return `VV${new Date().getFullYear().toString().slice(-2)}-${id}`
+  return `VV-${id}`
 }
--- test/unit/app/lib/create-reference.test.js
+++ test/unit/app/lib/create-reference.test.js
@@ -3,6 +3,6 @@ const createReference = require('../../../../app/lib/create-reference')
 describe('Test create reference number', () => {
   test('Returns reference number', async () => {
     const reference = createReference()
-    expect(reference.length).toBe(14)
+    expect(reference.length).toBe(12)
   })
 })
