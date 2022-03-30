63ca227dc23a3d594a73dfef33b7e20b47e5c24e
Shivanshu Patel <shivanshuit914@gmail.com>
2022-03-30 09:53:58 +0100

    feat: add contract tests for application process queue

---
+++ test/contract/process-message.test.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const { MessageConsumerPact, Matchers } = require('@pact-foundation/pact')
+const asbHelper = require('../asb-helper')
+const { set } = require('../../app/repositories/application-repository')
+const dbHelper = require('../db-helper')
+
+describe('receiving a new claim', () => {
+  let messagePact
+
+  beforeAll(async () => {
+    await asbHelper.clearAllSubscriptions()
+    await dbHelper.truncate()
+
+    messagePact = new MessageConsumerPact({
+      consumer: 'ffc-ahwr-application',
+      provider: 'ffc-ahwr-frontend',
+      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
+      dir: path.resolve(process.cwd(), 'test-output')
+    })
+  }, 30000)
+
+  afterAll(async () => {
+    await asbHelper.clearAllSubscriptions()
+    await dbHelper.close()
+  }, 30000)
+
+  test('new farmer application is received, saved', async () => {
+    await messagePact
+      .given('valid message')
+      .expectsToReceive('a request for new farmer application')
+      .withContent({
+        email: Matchers.email(),
+        reference: Matchers.like('VV-'),
+        type: Matchers.like('VV001'),
+        data: Matchers.like('{pigs: yes}'),
+        createdBy: Matchers.like('admin'),
+        updatedBy: Matchers.like('admin')
+      })
+      .withMetadata({
+        'content-type': 'application/json'
+      })
+      .verify(message => {
+        set(message.contents)
+      })
+  })
+})
