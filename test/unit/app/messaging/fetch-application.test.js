358b3f56a1b0696913a173447c347617546b4115
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-04 13:14:06 +0100

    feat: add unit tests to cover fetch application

---
+++ test/unit/app/messaging/fetch-application.test.js
@@ -0,0 +1,27 @@
+const fetchApplication = require('../../../../app/messaging/fetch-application')
+const applicationRepository = require('../../../../app/repositories/application-repository')
+const sendMessage = require('../../../../app/messaging/send-message')
+
+jest.mock('../../../../app/repositories/application-repository')
+jest.mock('../../../../app/messaging/send-message')
+
+describe(('Fetch application tests'), () => {
+  const message = {
+    body: {
+      application: 'VV-1234-5678',
+      sessionId: '8e5b5789-dad5-4f16-b4dc-bf6db90ce090'
+    }
+  }
+  const receiver = {
+    completeMessage: jest.fn(),
+    abandonMessage: jest.fn()
+  }
+
+  test('successfully fetched application', async () => {
+    await fetchApplication(message, receiver)
+    expect(applicationRepository.get).toHaveBeenCalledTimes(1)
+    expect(applicationRepository.get).toHaveBeenCalledWith(message.body.application)
+    expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
+    expect(sendMessage).toHaveBeenCalledTimes(1)
+  })
+})
