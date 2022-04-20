d1d153734dc0cf14660deb6d2975441a6fb99f70
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-20 15:55:44 +0100

    refactor: process vet visit unit test

--- test/unit/app/messaging/process-vet-visit.test.js
+++ test/unit/app/messaging/process-vet-visit.test.js
@@ -2,13 +2,20 @@ const processVetVisit = require('../../../../app/messaging/process-vet-visit')
 const vetVisitRepository = require('../../../../app/repositories/vet-visit-repository')
 const sendMessage = require('../../../../app/messaging/send-message')
 
-jest.mock('../../../../app/repositories/vet-visit-repository')
+vetVisitRepository.set = jest.fn().mockReturnValue({
+  dataValues: {
+    reference: '23D13'
+  }
+})
+
 jest.mock('../../../../app/messaging/send-message')
+jest.mock('../../../../app/repositories/vet-visit-repository')
 
 describe(('Store data in database'), () => {
   const message = {
     body: {
       applicationReference: 'VV-1234-5678',
+      rsvc: '13D2332',
       sessionId: '8e5b5789-dad5-4f16-b4dc-bf6db90ce090',
       refeerence: 'VV-1234-5678'
     }
@@ -17,6 +24,6 @@ describe(('Store data in database'), () => {
   test('successfully fetched application', async () => {
     await processVetVisit(message)
     expect(vetVisitRepository.set).toHaveBeenCalledTimes(1)
-    expect(sendMessage).toHaveBeenCalledTimes(0)
+    expect(sendMessage).toHaveBeenCalledTimes(1)
   })
 })
