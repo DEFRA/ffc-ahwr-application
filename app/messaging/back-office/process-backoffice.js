feced60094473707714380aef3c5757c9971c46a
Satish Chatap <satish.chatap@capgemini.com>
2022-06-18 23:22:35 +0100

    fixed error message & comments

--- app/messaging/back-office/process-backoffice.js
+++ app/messaging/back-office/process-backoffice.js
@@ -8,20 +8,18 @@ const processBackOfficeRequest = async (msg) => {
   const responseMessage = msg.body
   const { sessionId } = msg
   try {
-    console.log('BackOffice Request received:', util.inspect(msg.body, false, null, true))
-    // Get ID
-    const result = await getAll(msg.body.search.text, msg.body.limit ?? 10, msg.body.offset ?? 0)
+    // Get All Applications
+    const result = await getAll(msg.body.search.text, msg.body.limit, msg.body.offset)
     const total = await getApplicationCount(msg.body.search.text)
     if (result.length <= 0) {
       await sendMessage({ applicationState: states.notFound, applications: [], total: 0 }, backOfficeResponseMsgType, backOfficeResponseQueue, { sessionId })
     } else {
-    // Get All Applications
       await sendMessage({ applicationState: states.submitted, applications: result, total }, backOfficeResponseMsgType, backOfficeResponseQueue, { sessionId })
     }
   } catch (err) {
     console.error(err)
     responseMessage.error = {
-      message: 'can\'t submit applications at this time.'
+      message: 'unable to retrieve applications'
     }
     await sendMessage({ applicationState: states.notFound, applications: [], total: 0 }, backOfficeResponseMsgType, backOfficeResponseQueue, { sessionId })
   }
