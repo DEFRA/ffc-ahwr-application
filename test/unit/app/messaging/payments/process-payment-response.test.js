12f2876d9b3e95e9d146c27d4f13cad19f597725
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-06-20 08:28:12 +0100

    Fix to failing test and addition of new test

--- test/unit/app/messaging/payments/process-payment-response.test.js
+++ test/unit/app/messaging/payments/process-payment-response.test.js
@@ -15,12 +15,37 @@ describe(('Process payment response'), () => {
     jest.clearAllMocks()
   })
 
-  test('Sucessfuly update the payment with success status', async () => {
+  test('Sucessfully update the payment with success status', async () => {
     paymentRepository.updateByReference.mockResolvedValueOnce()
-    await processPaymentResponse({ body: { agreementNumber: 'AA-1234-567' } }, receiver)
+    await processPaymentResponse({
+      body: {
+        paymentRequest: {
+          agreementNumber: 'AA-1234-567'
+        },
+        accepted: true
+      }
+    }
+    , receiver)
 
     expect(paymentRepository.updateByReference).toHaveBeenCalledTimes(1)
     expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
+  })
+
+  test('Update the payment with failed status', async () => {
+    paymentRepository.updateByReference.mockResolvedValueOnce()
+    await processPaymentResponse({
+      body: {
+        paymentRequest: {
+          agreementNumber: 'AA-1234-567'
+        },
+        accepted: false
+      }
+    }
+    , receiver)
+
+    expect(paymentRepository.updateByReference).toHaveBeenCalledTimes(1)
+    expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
+    expect(consoleError).toHaveBeenCalledTimes(1)
   })
 
   test('console.error raised due to no agreement number within message', async () => {
@@ -28,6 +53,7 @@ describe(('Process payment response'), () => {
     await processPaymentResponse({}, receiver)
 
     expect(consoleError).toHaveBeenCalledTimes(1)
+    expect(receiver.deadLetterMessage).toHaveBeenCalledTimes(1)
   })
 
   test('console.error raised due to error thrown in updateByReference', async () => {
@@ -35,5 +61,6 @@ describe(('Process payment response'), () => {
     await processPaymentResponse({}, receiver)
 
     expect(consoleError).toHaveBeenCalledTimes(1)
+    expect(receiver.deadLetterMessage).toHaveBeenCalledTimes(1)
   })
 })
