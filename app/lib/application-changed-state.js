6ad0b6d15befc8d0e6e4f0fdbe0ef0d6f9c0f5fa
Julian Taylor <julian.taylor@capgemini.com>
2022-08-01 12:12:04 +0100

    Limit fields that are reported

--- app/lib/application-changed-state.js
+++ app/lib/application-changed-state.js
@@ -1,14 +1,32 @@
 const applicationChangedState = (applicationRecord) => {
   const attributes = Object.keys(applicationRecord.dataValues)
-  const originalState = {}
-  const newState = {}
+  const unwantedFields = [
+    'createdBy',
+    'updatedBy',
+    'reference',
+    'updatedAt'
+  ]
+  unwantedFields.forEach(unwantedField => {
+    attributes.splice(attributes.indexOf(unwantedField), 1)
+  })
+
+  let originalState = {}
+  let newState = {}
 
   attributes.forEach(field => {
-    if (applicationRecord.changed(field) && applicationRecord.previous(field) !== undefined) {
+    if (applicationRecord.changed(field) && applicationRecord[field] !== applicationRecord.previous(field)) {
       originalState[field] = applicationRecord.previous(field)
       newState[field] = applicationRecord[field]
     }
   })
+
+  if (Object.keys(originalState).length === 0) {
+    originalState = null
+  }
+
+  if (Object.keys(newState).length === 0) {
+    newState = null
+  }
 
   return { originalState, newState }
 }
