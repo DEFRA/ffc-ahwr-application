91d1a71e72865a55117ce239f3813a75bcec058a
Shivanshu Patel <shivanshuit914@gmail.com>
2022-09-14 14:11:33 +0100

    feat: add migration script to modify column size, fix unit test

--- test/unit/app/lib/create-reference.test.js
+++ test/unit/app/lib/create-reference.test.js
@@ -3,6 +3,6 @@ const { v4: uuid } = require('uuid')
 describe('Test create reference number', () => {
   test('Returns reference number', async () => {
     const reference = createReference(uuid())
-    expect(reference.length).toBe(12)
+    expect(reference.length).toBe(14)
   })
 })
