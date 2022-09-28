79c8d053ea053ea66d6142ec7fdbc04db3c8bcde
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-09-28 14:52:52 +0100

    Add carbon copy email send

--- app/config/notify.js
+++ app/config/notify.js
@@ -4,6 +4,7 @@ const notifyApiKeyRegex = new RegExp(`.*-${uuidRegex}-${uuidRegex}`)
 
 const schema = Joi.object({
   apiKey: Joi.string().pattern(notifyApiKeyRegex),
+  carbonCopyEmailAddress: Joi.string().email().allow(null, ''),
   templateIdFarmerApplicationComplete: Joi.string().uuid(),
   templateIdFarmerApplicationClaim: Joi.string().uuid(),
   templateIdFarmerClaimComplete: Joi.string().uuid(),
@@ -13,6 +14,7 @@ const schema = Joi.object({
 
 const config = {
   apiKey: process.env.NOTIFY_API_KEY,
+  carbonCopyEmailAddress: process.env.CARBON_COPY_EMAIL_ADDRESS,
   templateIdFarmerApplicationComplete: process.env.NOTIFY_TEMPLATE_ID_FARMER_APPLICATION_COMPLETE,
   templateIdFarmerApplicationClaim: process.env.NOTIFY_TEMPLATE_ID_FARMER_APPLICATION_CLAIM,
   templateIdFarmerClaimComplete: process.env.NOTIFY_TEMPLATE_ID_FARMER_CLAIM_COMPLETE,
