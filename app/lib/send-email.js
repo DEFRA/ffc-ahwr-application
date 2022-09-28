20ebe032da989b37724c84ca531467f4dd86eedc
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-28 14:26:46 +0100

    Add carbon copy email send

--- app/lib/send-email.js
+++ app/lib/send-email.js
@@ -1,21 +1,38 @@
 const notifyClient = require('./notify-client')
 const { serviceUri, applicationEmailDocRequestMsgType, applicationdDocCreationRequestQueue } = require('../config')
-const { templateIdVetApplicationComplete, templateIdFarmerApplicationClaim, templateIdFarmerClaimComplete, templateIdFarmerVetRecordIneligible } = require('../config').notify
+const { carbonCopyEmailAddress, templateIdVetApplicationComplete, templateIdFarmerApplicationClaim, templateIdFarmerClaimComplete, templateIdFarmerVetRecordIneligible } = require('../config').notify
 const sendMessage = require('../messaging/send-message')
+
+const send = async (templateId, email, personalisation) => {
+  return notifyClient.sendEmail(
+    templateId,
+    email,
+    personalisation
+  )
+}
 
 const sendEmail = async (email, personalisation, reference, templateId) => {
   let success = true
   try {
-    await notifyClient.sendEmail(
-      templateId,
-      email,
-      { personalisation, reference }
-    )
+    await send(templateId, email, { personalisation, reference })
+    await sendCarbonCopy(templateId, { personalisation, reference })
   } catch (e) {
     success = false
     console.error('Error occurred during sending email', e.response.data)
   }
   return success
+}
+
+const sendCarbonCopy = async (templateId, personalisation) => {
+  if (carbonCopyEmailAddress) {
+    await send(
+      templateId,
+      carbonCopyEmailAddress,
+      personalisation
+    )
+
+    console.log(`Carbon copy email sent to ${carbonCopyEmailAddress} for ${personalisation.reference}`)
+  }
 }
 
 const sendFarmerConfirmationEmail = async (reference, sbi, whichSpecies, startDate) => {
