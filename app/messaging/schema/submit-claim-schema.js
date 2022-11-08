475fa429b83aa7632a868e27dea5b895e44f4ca6
Shivanshu Patel <shivanshuit914@gmail.com>
2022-11-08 10:52:40 +0000

    feat: allow data to update application on submit claim (#55)
    * feat: allow data to update application on submit claim
    
    * bump: version
    
    * feat: improve schema validation
    
    * fix: linting

--- app/messaging/schema/submit-claim-schema.js
+++ app/messaging/schema/submit-claim-schema.js
@@ -2,7 +2,29 @@ const joi = require('joi')
 const util = require('util')
 
 const submitClaimSchema = joi.object({
-  reference: joi.string().required()
+  reference: joi.string().required(),
+  data: joi.object({
+    confirmCheckDetails: joi.string().required(),
+    whichReview: joi.string().required(),
+    eligibleSpecies: joi.string().required(),
+    reference: joi.string().allow(null).required(),
+    declaration: joi.boolean().required(),
+    offerStatus: joi.string().required(),
+    visitDate: joi.string().required(),
+    vetName: joi.string().required(),
+    urnResult: joi.string().required(),
+    vetRcvs: joi.string().required(),
+    detailsCorrect: joi.string().required(),
+    organisation: joi.object({
+      farmerName: joi.string().required(),
+      name: joi.string().required(),
+      sbi: joi.string().required(),
+      cph: joi.string().required(),
+      address: joi.string().required(),
+      email: joi.string().required(),
+      isTest: joi.boolean().optional()
+    })
+  })
 })
 
 const validateSubmitClaim = (event) => {
