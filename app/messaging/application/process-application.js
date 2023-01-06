e8bb3a7316523dee1d785c033d8d1f08eb48b5b9
govTechSatish <80264942+govTechSatish@users.noreply.github.com>
2023-01-06 17:06:23 +0000

    respond with app ref number if exists (#66)
    * respond with app ref number if exists
    
    * fixed tests
    
    * initialize value for existing application
    
    * standard fix

--- app/messaging/application/process-application.js
+++ app/messaging/application/process-application.js
@@ -9,6 +9,7 @@ const validateApplication = require('../schema/process-application-schema')
 const processApplication = async (msg) => {
   const { sessionId } = msg
   const applicationData = msg.body
+  let existingApplicationReference = null
   console.log('Application received:', util.inspect(applicationData, false, null, true))
   try {
     if (!validateApplication(applicationData)) {
@@ -19,6 +20,7 @@ const processApplication = async (msg) => {
       applicationData.organisation.sbi
     )
     if (existingApplication) {
+      existingApplicationReference = existingApplication.dataValues.reference
       throw Object.assign(
         new Error(
           `Application already exists: ${JSON.stringify({
@@ -66,7 +68,8 @@ const processApplication = async (msg) => {
     console.error('Failed to process application', error)
     sendMessage(
       {
-        applicationState: error.applicationState ? error.applicationState : states.failed
+        applicationState: error.applicationState ? error.applicationState : states.failed,
+        applicationReference: existingApplicationReference
       },
       applicationResponseMsgType,
       applicationResponseQueue,
