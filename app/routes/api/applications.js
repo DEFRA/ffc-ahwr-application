1a4890801f7d6cc9f50fe7f61c51b45736d9c079
Shivanshu Patel <shivanshuit914@gmail.com>
2023-01-30 21:21:15 +0000

    feat: add API endpout to withdraw application

--- app/routes/api/applications.js
+++ app/routes/api/applications.js
@@ -1,5 +1,7 @@
 const Joi = require('joi')
-const { get, searchApplications } = require('../../repositories/application-repository')
+const { get, searchApplications, updateByReference } = require('../../repositories/application-repository')
+const statusIds = require('../../constants/application-status');
+
 module.exports = [{
   method: 'GET',
   path: '/api/application/get/{ref}',
@@ -43,6 +45,33 @@ module.exports = [{
     handler: async (request, h) => {
       const { applications, total, applicationStatus } = await searchApplications(request.payload.search.text ?? '', request.payload.search.type, request.payload.filter, request.payload.offset, request.payload.limit, request.payload.sort)
       return h.response({ applications, total, applicationStatus }).code(200)
+    }
+  }
+}, {
+  method: 'PUT',
+  path: '/api/application/{ref}',
+  options: {
+    validate: {
+      params: Joi.object({
+        ref: Joi.string().valid()
+      }),
+      payload: Joi.object({
+        status: Joi.string(),
+        user: Joi.string(),
+      }),
+      failAction: async (_request, h, err) => {
+        return h.response({ err }).code(400).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      const application = (await get(request.params.ref))
+      if (!application.dataValues) {
+        return h.response('Not Found').code(404).takeover()
+      }
+
+      await updateByReference({ reference: request.params.ref, statusId: statusIds.withdrawn, updatedBy: user })
+
+      return h.response().code(200)
     }
   }
 }]
