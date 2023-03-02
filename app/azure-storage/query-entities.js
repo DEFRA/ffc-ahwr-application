857450ecbbe1b8587d9b937c106f9c10371154c8
Marcin Mogiela <marcinmo@kainos.com>
2023-03-02 12:30:22 +0100

    FFC-159409: added a test for queryEntitiesByPartitionKey

--- app/azure-storage/query-entities.js
+++ app/azure-storage/query-entities.js
@@ -2,6 +2,10 @@ const { odata } = require('@azure/data-tables')
 const createTableClient = require('./create-table-client')
 
 const queryEntitiesByPartitionKey = async (tableName, partitionKey) => {
+  console.log(`${new Date().toISOString()} Listing events by: ${JSON.stringify({
+    tableName,
+    partitionKey
+  })}`)
   const events = []
   if (tableName && partitionKey) {
     const tableClient = createTableClient(tableName)
