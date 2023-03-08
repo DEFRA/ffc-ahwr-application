4e35869195acf190ab9bcb35a17d2710244a9c8c
chrissmKainos <christophersm@kainos.com>
2023-03-08 09:29:20 +0000

    159409: Resolved lint issues

--- test/unit/app/azure-storage/application-status-repository.test.js
+++ test/unit/app/azure-storage/application-status-repository.test.js
@@ -10,7 +10,7 @@ describe('Application Status Repository test', () => {
   })
 
   describe('getApplicationHistory ', () => {
-    test('getApplicationHistory - application history found', async () => {     
+    test('getApplicationHistory - application history found', async () => {
       queryEntities.mockResolvedValue(
         [{ ChangedOn: '2023-03-23T10:00:12.000Z', Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 91\n}', ChangedBy: 'Daniel Jones' },
           { ChangedOn: '2023-03-24T09:30:00.000Z', Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 2\n}', ChangedBy: 'Daniel Jones' },
@@ -20,15 +20,13 @@ describe('Application Status Repository test', () => {
       expect(queryEntities).toHaveBeenCalledTimes(1)
       expect(result).not.toBeNull()
       expect(result.length).toBe(3)
-
-    }),
+    })
     test('getApplicationHistory - application history not found', async () => {
-        queryEntities.mockResolvedValue([])
-        const result = await repository.getApplicationHistory(reference)
-        
-        expect(queryEntities).toHaveBeenCalledTimes(1)
-        expect(result).toBeNull()
-      })
-  })
+      queryEntities.mockResolvedValue([])
+      const result = await repository.getApplicationHistory(reference)
 
+      expect(queryEntities).toHaveBeenCalledTimes(1)
+      expect(result).toBeNull()
+    })
+  })
 })
