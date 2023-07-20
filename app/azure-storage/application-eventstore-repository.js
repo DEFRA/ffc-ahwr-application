384447555de2773ef590e4b44f507407f36d533c
chrissmKainos <christophersm@kainos.com>
2023-07-20 13:05:33 +0100

    211810 - Update following PR comment

--- app/azure-storage/application-eventstore-repository.js
+++ app/azure-storage/application-eventstore-repository.js
@@ -1,11 +1,13 @@
 const queryEntitiesByPartitionKey = require('./query-entities')
 const { odata } = require('@azure/data-tables')
 
-const getApplicationEvents = async (reference) => {
+const getApplicationEvents = async (sbi) => {
   const eventRecords = await queryEntitiesByPartitionKey(
     'ahwreventstore',
-    reference,
-    odata`PartitionKey ge ${reference} and PartitionKey lt ${(+reference + 1).toString()}`
+    sbi,
+    // The partition key in the eventstore table can be either sbi or sbi_cph
+    // so query where the partition key starts with the sbi
+    odata`PartitionKey ge ${sbi} and PartitionKey lt ${(+sbi + 1).toString()}`
   )
 
   if (eventRecords.length === 0) { return null }
--- app/azure-storage/application-status-repository.js
+++ app/azure-storage/application-status-repository.js
@@ -5,6 +5,8 @@ const getApplicationHistory = async (reference) => {
   const historyRecords = await queryEntitiesByPartitionKey(
     'ffcahwrapplicationstatus',
     reference,
+    // The partition key in the application status table is the application reference
+    // so query where it's an exact match
     odata`PartitionKey eq ${reference}`
   )
 
