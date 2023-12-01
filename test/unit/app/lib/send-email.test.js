0daed3f8f460603ae660c155afd671f127c717c0
JulieLev <JulieLev@users.noreply.github.com>
2023-12-01 19:29:15 +0000

    extended send-email tests

--- test/unit/app/lib/send-email.test.js
+++ test/unit/app/lib/send-email.test.js
@@ -1,6 +1,7 @@
 const sendEmail = require('../../../../app/lib/send-email')
 const { applicationEmailDocRequestMsgType, applicationdDocCreationRequestQueue } = require('../../../../app/config')
 const { templateIdFarmerClaimComplete } = require('../../../../app/config').notify
+const appInsights = require('applicationinsights')
 
 const error = new Error('Test exception')
 error.response = { data: 'failed to send email' }
@@ -31,7 +32,7 @@ describe('Send email test', () => {
     expect(sendMessage).toHaveBeenCalledWith({ reference, sbi, whichSpecies, startDate, email, farmerName }, applicationEmailDocRequestMsgType, applicationdDocCreationRequestQueue)
   })
 
-  test('sendFarmerClaimConfirmationEmail returns true onsuccessful email', async () => {
+  test('sendFarmerClaimConfirmationEmail returns true on successful email', async () => {
     notifyClient.sendEmail.mockResolvedValueOnce(true)
 
     const response = await sendEmail.sendFarmerClaimConfirmationEmail(email, reference)
@@ -40,11 +41,28 @@ describe('Send email test', () => {
     expect(response).toBe(true)
   })
 
+  test('sendFarmerClaimConfirmationEmail tracks an event on successful email', async () => {
+    notifyClient.sendEmail.mockResolvedValueOnce(true)
+
+    await sendEmail.sendFarmerClaimConfirmationEmail(email, reference)
+
+    expect(appInsights.defaultClient.trackEvent).toHaveBeenCalledTimes(1)
+  })
+
   test('sendFarmerClaimConfirmationEmail returns false on error sending email', async () => {
     notifyClient.sendEmail.mockRejectedValueOnce(error)
 
     const response = await sendEmail.sendFarmerClaimConfirmationEmail(email, reference)
 
     expect(response).toBe(false)
+  })
+
+  test('sendFarmerClaimConfirmationEmail tracks an error on error sending email', async () => {
+    notifyClient.sendEmail.mockRejectedValueOnce(error)
+
+    await sendEmail.sendFarmerClaimConfirmationEmail(email, reference)
+
+    expect(appInsights.defaultClient.trackException).toHaveBeenCalledTimes(1)
+    expect(appInsights.defaultClient.trackException).toHaveBeenCalledWith({ exception: error })
   })
 })
