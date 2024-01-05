b9ea38eea47e8766cdc38d7b27bc262333baa911
Alex White <alex.a.white@capgemini.com>
2024-01-05 09:54:04 +0000

    Add endemics feature flag

--- app/messaging/schema/process-application-schema.js
+++ app/messaging/schema/process-application-schema.js
@@ -1,6 +1,26 @@
 const joi = require('joi')
+const endemicsEnabled = require('../../config/index').endemics.enabled
 
 const applicationSchema = joi.object({
+  confirmCheckDetails: joi.string().required(),
+  whichReview: joi.string().required(),
+  eligibleSpecies: joi.string().required(),
+  reference: joi.string().allow(null).required(),
+  declaration: joi.boolean().required(),
+  offerStatus: joi.string().required(),
+  organisation: joi.object({
+    farmerName: joi.string().required(),
+    name: joi.string().required(),
+    sbi: joi.string().required(),
+    cph: joi.string().optional(),
+    crn: joi.string().optional(),
+    address: joi.string().required(),
+    email: joi.string().required().lowercase().email({ tlds: false }),
+    isTest: joi.boolean().optional()
+  })
+})
+
+const endemicsApplicationSchema = joi.object({
   confirmCheckDetails: joi.string().required(),
   whichReview: joi.string().optional(),
   eligibleSpecies: joi.string().optional(),
@@ -21,7 +41,7 @@ const applicationSchema = joi.object({
 })
 
 const validateApplication = (event) => {
-  const validate = applicationSchema.validate(event)
+  const validate = endemicsEnabled ? endemicsApplicationSchema.validate(event) : applicationSchema.validate(event)
 
   if (validate.error) {
     console.error(`Application validation error - ${validate.error}.`)
