6a293f0835ab00943b75af187ca703f40e3f1bc3
takano <tosin.akano@capgemini.com>
2024-02-20 10:17:10 +0000

    replace que with direct REST API call

--- app/routes/api/applications.js
+++ app/routes/api/applications.js
@@ -2,7 +2,6 @@ const Joi = require('joi')
 const { v4: uuid } = require('uuid')
 const { get, searchApplications, updateByReference } = require('../../repositories/application-repository')
 const { submitPaymentRequestMsgType, submitRequestQueue } = require('../../config')
-const sendMessage = require('../../messaging/send-message')
 const statusIds = require('../../constants/application-status')
 
 module.exports = [{
@@ -102,14 +101,6 @@ module.exports = [{
         let statusId = statusIds.rejected
         if (request.payload.approved) {
           statusId = statusIds.readyToPay
-
-          await sendMessage(
-            {
-              reference: request.payload.reference,
-              sbi: application.dataValues.data.organisation.sbi,
-              whichReview: application.dataValues.data.whichReview
-            }, submitPaymentRequestMsgType, submitRequestQueue, { sessionId: uuid() }
-          )
         }
 
         await updateByReference({ reference: request.payload.reference, statusId, updatedBy: request.payload.user })
@@ -121,4 +112,5 @@ module.exports = [{
       return h.response().code(200)
     }
   }
-}]
+}
+]
