465537abcbeab147464826a7434e5c45fb9ab293
Alex White <alex_white742@outlook.com>
2024-03-14 12:45:59 +0000

    340923 if endemics then only check endemics apps 0.44.32 (#177)
    * if endemics then only check endemics apps 0.44.32
    
    * 0.44.33
    
    * Test coverage for endemics flag

--- test/unit/app/messaging/application/process-application.test.js
+++ test/unit/app/messaging/application/process-application.test.js
@@ -22,10 +22,14 @@ jest.mock('../../../../../app/config', () => ({
     enabled: false
   }
 }))
-const { applicationResponseMsgType, applicationResponseQueue, tenMonthRule } = require('../../../../../app/config')
+const { applicationResponseMsgType, applicationResponseQueue, tenMonthRule, endemics } = require('../../../../../app/config')
 
 const toggle10Months = (toggle) => {
   tenMonthRule.enabled = toggle
+}
+
+const toggleEndemics = (toggle) => {
+  endemics.enabled = toggle
 }
 
 jest.mock('../../../../../app/lib/send-email')
@@ -132,8 +136,88 @@ describe(('Store application in database'), () => {
       expect(sendMessage).toHaveBeenCalledWith({ applicationState: states.submitted, applicationReference: MOCK_REFERENCE }, applicationResponseMsgType, applicationResponseQueue, { sessionId })
     })
 
+    describe('when endemics toggle is enabled', () => {
+      beforeEach(() => toggleEndemics(true))
+      test('throws an error when existing endemics application', async () => {
+        const mockApplicationDate = mockMonthsAgo(30)
+
+        when(applicationRepository.getBySbi)
+          .calledWith(
+            message.body.organisation.sbi
+          )
+          .mockResolvedValue({
+            dataValues: {
+              reference: MOCK_REFERENCE
+            },
+            statusId: applicationStatus.readyToPay,
+            createdAt: mockApplicationDate,
+            type: 'EE'
+          })
+
+        await processApplication(message)
+
+        expect(applicationRepository.set).toHaveBeenCalledTimes(0)
+        expect(sendFarmerConfirmationEmail).toHaveBeenCalledTimes(0)
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          'Failed to process application',
+          new Error(`Recent application already exists: ${JSON.stringify({
+            reference: MOCK_REFERENCE,
+            createdAt: mockApplicationDate
+          })}`)
+        )
+        expect(sendMessage).toHaveBeenCalledTimes(1)
+        expect(sendMessage).toHaveBeenCalledWith(
+          {
+            applicationState: states.alreadyExists,
+            applicationReference: MOCK_REFERENCE
+          },
+          applicationResponseMsgType,
+          applicationResponseQueue,
+          {
+            sessionId
+          }
+        )
+      })
+
+      test('submits and does not throw an error with statusId 9 (ready to pay) and date more than 10 months ago', async () => {
+        const mockApplicationDate = mockMonthsAgo(11)
+        when(applicationRepository.getBySbi)
+          .calledWith(
+            message.body.organisation.sbi
+          )
+          .mockResolvedValue({
+            dataValues: {
+              reference: MOCK_REFERENCE
+            },
+            createdAt: mockApplicationDate,
+            statusId: applicationStatus.readyToPay,
+            type: 'VV'
+          })
+
+        await processApplication(message)
+
+        expect(applicationRepository.set).toHaveBeenCalledTimes(1)
+        expect(sendFarmerConfirmationEmail).toHaveBeenCalledTimes(1)
+        expect(sendMessage).toHaveBeenCalledTimes(1)
+        expect(sendMessage).toHaveBeenCalledWith(
+          {
+            applicationState: states.submitted,
+            applicationReference: MOCK_REFERENCE
+          },
+          applicationResponseMsgType,
+          applicationResponseQueue,
+          {
+            sessionId
+          }
+        )
+      })
+    })
+
     describe('when 10 month rule toggle is enabled', () => {
-      beforeEach(() => toggle10Months(true))
+      beforeEach(() => {
+        toggle10Months(true)
+        toggleEndemics(false)
+      })
       test('throws an error with statusId 9 (ready to pay) and date less than 10 months ago', async () => {
         const mockApplicationDate = mockMonthsAgo(7)
 
@@ -208,7 +292,10 @@ describe(('Store application in database'), () => {
     })
 
     describe('when 10 month rule toggle is not enabled', () => {
-      beforeEach(() => toggle10Months(false))
+      beforeEach(() => {
+        toggle10Months(false)
+        toggleEndemics(false)
+      })
 
       test('throws an error with statusId 9 (ready to pay) and date less than 10 months ago', async () => {
         const mockApplicationDate = mockMonthsAgo(7)
