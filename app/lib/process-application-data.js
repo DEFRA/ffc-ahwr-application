48a5cc2da9c1ed37b5f2b0cfc7bf4345b630715a
takano <tosin.akano@capgemini.com>
2024-03-24 15:08:40 +0000

    queue replacement for application

---
+++ app/lib/process-application-data.js
@@ -0,0 +1,122 @@
+const validateApplication = require("../messaging/schema/process-application-schema")
+const { sendFarmerConfirmationEmail } = require("./send-email")
+const {endemics, tenMonthRule} = require("../config")
+const applicationRepository = require("../repositories/application-repository")
+const appInsights = require('applicationinsights')
+const states = require("../messaging/application/states")
+
+function timeLimitDates (application) {
+  const start = new Date(application.createdAt)
+  const end = new Date(start)
+  // refactor to set time limit to a constant - config??
+  end.setMonth(end.getMonth() + 10)
+  end.setHours(23, 59, 59, 999) // set to midnight of agreement end day
+  return { startDate: start, endDate: end }
+}
+
+function isPastTimeLimit (dates) {
+  const { endDate } = dates
+  return Date.now() > endDate
+}
+
+function isPreviousApplicationRelevant (existingApplication) {
+  if (endemics.enabled) {
+    if (existingApplication?.type === 'EE') {
+      return true
+    }
+    return false
+  } else if (tenMonthRule.enabled) {
+    return existingApplication &&
+    ((existingApplication.statusId !== applicationStatus.withdrawn &&
+    existingApplication.statusId !== applicationStatus.notAgreed &&
+    // check if it passes 10 month rule here and chuck error if it doesn't
+    isPastTimeLimit(timeLimitDates(existingApplication)) === false) ||
+    existingApplication.statusId === applicationStatus.agreed)
+  } else {
+    return existingApplication &&
+    existingApplication.statusId !== applicationStatus.withdrawn &&
+    existingApplication.statusId !== applicationStatus.notAgreed
+  }
+}
+
+/**
+ * processApplicationData
+ * */
+const processApplicationData =  async (applicationData, sessionId) => {
+
+    const existingApplicationReference = null
+
+try{
+    
+    if (!validateApplication(applicationData)) {
+        throw new Error('Application validation error')
+      }
+
+      console.log(`Application received : ${JSON.stringify(applicationData)} with sessionId ${sessionId}`)
+
+    const existingApplication = await applicationRepository.getBySbi(
+        applicationData.organisation.sbi
+      )
+      console.log('existingApplication=====>', existingApplication) 
+  
+      if (isPreviousApplicationRelevant(existingApplication)) {
+        existingApplicationReference = existingApplication.dataValues.reference
+        throw Object.assign(
+          new Error(
+            `Recent application already exists: ${JSON.stringify({
+              reference: existingApplication.dataValues.reference,
+              createdAt: existingApplication.createdAt
+            })}`
+          ),
+          {
+            applicationState: states
+            .alreadyExists
+          }
+        )
+      }
+      
+      // set the Data structure
+
+    const result = await applicationRepository.set({
+      reference: '',
+      data: applicationData,
+      createdBy: 'admin',
+      createdAt: new Date(),
+      statusId: applicationData.offerStatus === 'rejected' ? 7 : 1,
+      type: applicationData.type ? applicationData.type : 'VV'
+    })
+    const application = result.dataValues
+    console.log("application=====>", application)
+    console.log('result from applicationRepository.set ====>', result)
+    console.log('only data values result====>', result.dataValues)
+
+      //
+      const {data, organisation, createdBy, createdAt, whichReview, statusId, type} = applicationData || {}
+      console.log('data for email ====>', {data, organisation, createdBy, createdAt, whichReview, statusId, type})
+
+    //send email
+    applicationData.offerStatus === 'accepted' && await sendFarmerConfirmationEmail(application.reference, organisation.sbi, whichReview, createdAt, organisation.email, organisation.farmerName)
+    
+    
+    // track events 
+    appInsights.defaultClient.trackEvent({
+        name: 'process-application',
+        properties: {
+          status: applicationData?.offerStatus,
+          reference: application ? application?.reference : 'unknown',
+          sbi: applicationData?.organisation?.sbi,
+          sessionId
+        }
+      })
+
+      return result;
+
+    } catch (error) {
+        console.error('Failed to process application', error)
+        appInsights.defaultClient.trackException({ exception: error })
+          }
+}
+
+module.exports = {
+    processApplicationData
+}
