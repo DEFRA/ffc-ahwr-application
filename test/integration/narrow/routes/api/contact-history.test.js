b04dd2b7e6a77a8f38fdeb306c3093be707f1e93
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-04-14 18:01:19 +0100

    add more test coverage for contact history route

--- test/integration/narrow/routes/api/contact-history.test.js
+++ test/integration/narrow/routes/api/contact-history.test.js
@@ -17,7 +17,7 @@ describe('Update contact history test', () => {
     await server.stop()
   })
 
-  test.only('Update email and address fields in application and add the changes fields to contact history table', async () => {
+  test('Update email and address fields in application and add the changes fields to contact history table', async () => {
     const options = {
       method: 'PUT',
       url: '/api/application/contact-history',
@@ -65,4 +65,73 @@ describe('Update contact history test', () => {
     expect(contactHistory.set).toHaveBeenCalledTimes(2)
   })
 
+  test('Return 200 without updating contact history if no application found for the sbi', async () => {
+    const options = {
+      method: 'PUT',
+      url: '/api/application/contact-history',
+      payload: {
+        user: 'admin',
+        email: 'test@example.com',
+        address: '20 Everest Road,Rectory Road,BRICKHILL HOUSE,BLACKTON,TOTNES,CH64 6RT,United Kingdom',
+        sbi: 107544286
+      }
+    }
+
+    applicationRepository.getLatestApplicationsBySbi.mockResolvedValue([])
+
+    const res = await server.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    expect(applicationRepository.updateByReference).toHaveBeenCalledTimes(0)
+    expect(res.payload).toBe('No applications found to update')
+    expect(contactHistory.set).toHaveBeenCalledTimes(0)
+  })
+
+  test('Will not call set contact history and updateByReference if there is no change in email and the address', async () => {
+    const options = {
+      method: 'PUT',
+      url: '/api/application/contact-history',
+      payload: {
+        user: 'admin',
+        email: 'test@example.com',
+        address: '20 Everest Road,Rectory Road,BRICKHILL HOUSE,BLACKTON,TOTNES,CH64 6RT,United Kingdom',
+        sbi: 107544286
+      }
+    }
+
+    applicationRepository.getLatestApplicationsBySbi.mockResolvedValue([
+      {
+        id: '90496416-0c95-46e5-a79b-5d29ef8ebc39',
+        reference: 'AHWR-9049-6416',
+        data: {
+          reference: null,
+          declaration: true,
+          offerStatus: 'accepted',
+          whichReview: 'dairy',
+          organisation: {
+            sbi: 107204504,
+            name: 'West Somerset Advice Bureau',
+            email: 'test@example.com',
+            address: '20 Everest Road,Rectory Road,BRICKHILL HOUSE,BLACKTON,TOTNES,CH64 6RT,United Kingdom',
+            orgEmail: 'westsomersetadvicebureaux@uaerubecivdatesremostsewo.com.test',
+            farmerName: 'Hayley Penrose-Body'
+          },
+          eligibleSpecies: 'yes',
+          confirmCheckDetails: 'yes'
+        },
+        claimed: false,
+        createdAt: '2024-04-12T11:21:56.187Z',
+        updatedAt: '2024-04-12T14:54:55.736Z',
+        createdBy: 'admin',
+        updatedBy: 'admin',
+        statusId: 1,
+        type: 'VV'
+      }])
+
+    const res = await server.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    expect(applicationRepository.updateByReference).toHaveBeenCalledTimes(0)
+    expect(contactHistory.set).toHaveBeenCalledTimes(0)
+  })
 })
