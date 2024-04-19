690b822e8a829f21d76205917524e2b73a35e9a4
Afshin <147500504+Afshin-Karamifar-Capgemini@users.noreply.github.com>
2024-04-19 09:53:32 +0100

    344194 - All claims status will be in-check after submission (#198)
    * All claims status will be in-check after submission
    
    * Version improved
    
    * Ignore Sonarcloud
    
    * Ignore Sonarcloud
    
    * Ignore Sonarcloud
    
    * Ignore Sonarcloud
    
    * Ignore Sonarcloud
    
    * Ignore Sonarcloud

--- app/lib/requires-compliance-check.js
+++ app/lib/requires-compliance-check.js
@@ -1,4 +1,6 @@
+const { compliance } = require('../config')
 const statusIds = require('../constants/application-status')
+const { getAllClaimedClaims } = require('../repositories/claim-repository')
 const { getAllClaimedApplications } = require('../repositories/application-repository')
 
 /**
@@ -8,14 +10,21 @@ const { getAllClaimedApplications } = require('../repositories/application-repos
  * @param {*} complianceCheckRatio the ratio of compliance checks e.g. 3 indicates one in every 3 claims are checked
  * @returns an object containing a statusId and a claimed indicator
  */
-module.exports = async function requiresComplianceCheck (claimStatusIds, complianceCheckRatio) {
-  const claimedApplicationsCount = await getAllClaimedApplications(claimStatusIds)
+module.exports = async function requiresComplianceCheck (claimOrApplication) {
+  const claimStatusIds = [statusIds.inCheck, statusIds.readyToPay, statusIds.rejected, statusIds.onHold, statusIds.recommendToPay, statusIds.recommendToReject]
+
+  let claimedApplicationsCount
+  if (claimOrApplication === 'claim') {
+    claimedApplicationsCount = await getAllClaimedClaims(claimStatusIds)
+  } else {
+    claimedApplicationsCount = await getAllClaimedApplications(claimStatusIds)
+  }
 
   // default to IN_CHECK status
   let statusId = statusIds.inCheck
   let claimed = false
 
-  if (complianceCheckRatio <= 0 || (claimedApplicationsCount + 1) % complianceCheckRatio !== 0) {
+  if (compliance.complianceCheckRatio <= 0 || (claimedApplicationsCount + 1) % compliance.complianceCheckRatio !== 0) {
     // if the claim does not trigger the configurable compliance check volume ratio set as onHold
     // if complianceCheckRatio is 0 or less this means compliance checks are turned off
     statusId = statusIds.onHold
