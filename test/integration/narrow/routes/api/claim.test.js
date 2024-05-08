9dded08df671a1ce733c9c0cd8055f46f118554a
takano <tosin.akano@capgemini.com>
2024-05-08 13:54:37 +0100

    additional test coverage

--- test/integration/narrow/routes/api/claim.test.js
+++ test/integration/narrow/routes/api/claim.test.js
@@ -341,10 +341,66 @@ describe('Post claim test', () => {
     await sendEmail.sendFarmerEndemicsClaimConfirmationEmail(mockEmailData)
 
     await server.inject(options)
+
     expect(claimRepository.set).toHaveBeenCalledTimes(1)
     expect(sendEmail.sendFarmerEndemicsClaimConfirmationEmail).toHaveBeenCalledTimes(1)
     expect(Object.keys(mockEmailData)).toEqual(expect.arrayContaining(['reference', 'email', 'amount', 'farmerName', 'orgData']))
     expect(sendEmail.sendFarmerEndemicsClaimConfirmationEmail).toHaveBeenCalledWith(expect.objectContaining(mockEmailData))
+  })
+  test('contain corresponding values ', async () => {
+    const options = {
+      method: 'POST',
+      url: '/api/claim',
+      payload: { ...claim }
+    }
+
+    applicationRepository.get.mockResolvedValue({
+      dataValues: {
+        createdAt: '2024-02-14T09:59:46.756Z',
+        id: '0f5d4a26-6a25-4f5b-882e-e18587ba9f4b',
+        updatedAt: '2024-02-14T10:43:03.544Z',
+        updatedBy: 'admin',
+        reference: 'AHWR-0F5D-4A26',
+        applicationReference: 'AHWR-0AD3-3322',
+        data: {
+          vetsName: 'Afshin',
+          dateOfVisit: '2024-01-22T00:00:00.000Z',
+          testResults: 'positive',
+          typeOfReview: 'review one',
+          dateOfTesting: '2024-01-22T00:00:00.000Z',
+          laboratoryURN: 'AK-2024',
+          vetRCVSNumber: 'AK-2024',
+          speciesNumbers: 'yes',
+          typeOfLivestock: 'pigs',
+          numberAnimalsTested: 30,
+          amount: '£[amount]'
+        },
+        statusId: 1,
+        type: 'R',
+        createdBy: 'admin'
+      }
+    })
+    const mockEmailData = {
+      reference: 'AHWR-0F5D-4A26',
+      email: 'test@test-unit.com',
+      amount: '£[amount]',
+      farmerName: 'farmerName',
+      orgData: { orgName: 'orgName', orgEmail: 'test@test-unit.org' }
+    }
+
+    await sendEmail.sendFarmerEndemicsClaimConfirmationEmail(mockEmailData)
+
+    const { reference, email, amount, farmerName, orgData } = mockEmailData || {}
+
+    await server.inject(options)
+
+    expect(claimRepository.set).toHaveBeenCalledTimes(1)
+    expect(sendEmail.sendFarmerEndemicsClaimConfirmationEmail).toHaveBeenCalledTimes(1)
+    expect(reference).toMatch('AHWR-0F5D-4A26')
+    expect(email).toMatch('test@test-unit.com')
+    expect(amount).toMatch('£[amount]')
+    expect(farmerName).toMatch('farmerName')
+    expect(orgData).toEqual(expect.objectContaining({ orgName: 'orgName', orgEmail: 'test@test-unit.org' }))
   })
 })
 
