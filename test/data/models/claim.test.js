763548dfff447299270be3da0d1f19797342c559
takano <tosin.akano@capgemini.com>
2024-05-17 11:20:31 +0100

    add claim model hooks tests

--- test/data/models/claim.test.js
+++ test/data/models/claim.test.js
@@ -10,7 +10,10 @@ jest.mock('../../../app/lib/generate-pre-text-for-claim')
 const mockSequelize = {
   define: jest.fn().mockReturnValue({
     create: jest.fn(),
-    associate: jest.fn()
+    associate: jest.fn(),
+    hooks: {
+      afterCreate: jest.fn()
+    }
   }),
   UUIDV4: 'mock-uuid-v4'
 }
@@ -77,16 +80,20 @@ describe('claim model', () => {
   test('should call afterCreate hook with the correct logic', async () => {
     const mockCreateAgreementNumber = jest.fn().mockReturnValue('REBC-1234-2345')
     const mockCreateReference = jest.fn().mockReturnValue('AHWR-1234-APP1')
+    const mockGenerateClaimPreText = jest.fn().mockReturnValue('REBC')
     const mockEndemics = {
       enabled: true
     }
-    mockCreateAgreementNumber()
     const mockClaimRecord = {
       id: 'mock-id',
       dataValues: {
-        reference: 'REBC-1234-2345'
+        reference: 'REBC-1234-2345',
+        update: jest.fn()
       },
-      update: jest.fn()
+      data: {
+        typeOfLiveStock: 'beef'
+      },
+      type: 'R'
     }
     const mockModels = {
       status: {
@@ -94,17 +101,33 @@ describe('claim model', () => {
         foreignKey: 'statusId'
       }
     }
-    Claim.hooks = {
-      afterCreate: jest.fn()
-    }
+    const _ = {}
+    Claim.hooks.afterCreate.mockImplementation(async (mockClaimRecord, _) => {
+      mockClaimRecord.dataValues.reference = mockEndemics.enabled
+        ? mockCreateAgreementNumber(mockGenerateClaimPreText(mockClaimRecord.type, mockClaimRecord.dataValues?.data?.typeOfLivestock || ''))
+        : mockCreateReference(mockClaimRecord.id)
+      mockClaimRecord.dataValues.updatedBy = 'admin'
+      mockClaimRecord.dataValues.updatedAt = new Date()
+
+      mockClaimRecord.update = jest.fn().mockImplementation((data) => {
+        mockClaimRecord.dataValues = data
+      })
+
+      await mockClaimRecord.update(mockClaimRecord.dataValues)
+    })
+
+    await Claim.hooks.afterCreate(mockClaimRecord, _)
 
     Claim.associate(mockModels)
     Claim.hooks.afterCreate(mockClaimRecord, mockSequelize)
     expect(mockEndemics.enabled).toBe(true)
-    expect(mockCreateAgreementNumber).toHaveBeenCalledTimes(1)
+    expect(mockCreateAgreementNumber).toHaveBeenCalled()
     expect(mockCreateReference).toHaveBeenCalledTimes(0)
     expect(mockClaimRecord.dataValues.reference).toMatch('REBC-1234-2345')
     expect(mockClaimRecord.dataValues.reference).toMatch(mockClaimRecord.dataValues.reference.toUpperCase())
+    expect(mockClaimRecord.update).toHaveBeenCalledWith(mockClaimRecord.dataValues)
+    expect(mockClaimRecord.dataValues.updatedBy).toMatch('admin')
+    expect(mockClaimRecord.dataValues.updatedAt).toBeDefined()
   })
   test('should call createReference to create reference number', async () => {
     const mockCreateAgreementNumber = jest.fn().mockReturnValue('REBC-1234-2345')
