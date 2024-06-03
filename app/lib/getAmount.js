759d8495efa196653adb119d621f5dbbc8fd6e7d
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-06-03 06:27:12 +0100

    add review claims payment

--- app/lib/getAmount.js
+++ app/lib/getAmount.js
@@ -1,10 +1,12 @@
 const { livestockTypes } = require('./../constants/claim')
 
-const getAmount = (typeOfLivestock, testResults, pricesConfig) => {
-  if ((typeOfLivestock === livestockTypes.beef || typeOfLivestock === livestockTypes.dairy) && testResults) {
-    return String(pricesConfig[typeOfLivestock].value[testResults])
+const getAmount = (typeOfLivestock, testResults, pricesConfig, isReview, isEndemics) => {
+  const claimType = isReview ? 'review' : 'followUp'
+
+  if ((typeOfLivestock === livestockTypes.beef || typeOfLivestock === livestockTypes.dairy) && testResults && isEndemics) {
+    return pricesConfig[claimType][typeOfLivestock].value[testResults]
   } else {
-    return String(pricesConfig[typeOfLivestock].value)
+    return pricesConfig[claimType][typeOfLivestock].value
   }
 }
 
