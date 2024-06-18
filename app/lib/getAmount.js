92bcefea7e6cfa342c9687c2846babd0ef77331a
Alex White <alex.a.white@capgemini.com>
2024-06-18 14:50:07 +0100

    Logging payment

--- app/lib/getAmount.js
+++ app/lib/getAmount.js
@@ -1,6 +1,7 @@
 const { livestockTypes } = require('./../constants/claim')
 
 const getAmount = (typeOfLivestock, testResults, pricesConfig, isReview, isEndemics) => {
+  console.log('getAmount:', 'typeOfLivestock', typeOfLivestock, 'testResults', testResults, 'pricesConfig', pricesConfig, 'isReview', isReview, 'isEndemics', isEndemics)
   const claimType = isReview ? 'review' : 'followUp'
 
   if ((typeOfLivestock === livestockTypes.beef || typeOfLivestock === livestockTypes.dairy) && testResults && isEndemics) {
