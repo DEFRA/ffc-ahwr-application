8ccf45102d04ba59aaad31c1c7e46c0994cb4274
Alex White <alex.a.white@capgemini.com>
2024-08-16 10:11:35 +0100

    optionalPiHunt prices

--- app/lib/getAmount.js
+++ app/lib/getAmount.js
@@ -1,9 +1,15 @@
 const { livestockTypes } = require('./../constants/claim')
 
-const getAmount = (typeOfLivestock, testResults, pricesConfig, isReview, isEndemics) => {
+const getAmount = (typeOfLivestock, testResults, pricesConfig, isReview, isEndemics, optionalPiHunt) => {
+  // maybe pass in the full request data instead of specifics so we can get all the piHunt data
+  // to check for a valid piHunt we only need to check if they said YES to piHuntAllAnimals
+  // we can also move the get request for the prices config to this file as it is only used here
   const claimType = isReview ? 'review' : 'followUp'
 
-  if ((typeOfLivestock === livestockTypes.beef || typeOfLivestock === livestockTypes.dairy) && testResults && isEndemics) {
+  if ((typeOfLivestock === livestockTypes.beef || typeOfLivestock === livestockTypes.dairy) && testResults && isEndemics && optionalPiHunt) {
+    if (testResults === 'negative') {
+      return pricesConfig[claimType][typeOfLivestock].value[testResults][optionalPiHunt]
+    }
     return pricesConfig[claimType][typeOfLivestock].value[testResults]
   } else {
     return pricesConfig[claimType][typeOfLivestock].value
