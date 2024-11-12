5ad4e93968c53151aba69944805ee40b1ed56258
Ben Hope <bendef24@gmail.com>
2024-11-12 22:37:31 +0000

    feat(AHWR-202): fixed linting issues

--- app/lib/sfd-client.js
+++ app/lib/sfd-client.js
@@ -1,29 +1,27 @@
-const util = require('util')
-const config = require('../config')
 const sendMessage = require('../messaging/send-message')
 const { sfdRequestMsgType, sfdMessageQueue } = require('../config')
 const validateSFDClaim = require('../messaging/schema/submit-sfd-schema')
 const states = require('../messaging/application/states')
 
 const sendSFDEmail = async (templateId, email, personalisation) => {
-    const { personalisation: { reference, applicationReference, crn, sbi } } = personalisation
+  const { personalisation: { reference, applicationReference, crn, sbi } } = personalisation
 
-    let sfdMessage = {
-        crn: crn,
-        sbi: sbi,
-        agreementReference: applicationReference,
-        claimReference: reference,
-        notifyTemplateId: templateId,
-        emailAddress: email,
-        customParams: personalisation,
-        dateTime: new Date().toISOString()
-    }
+  const sfdMessage = {
+    crn,
+    sbi,
+    agreementReference: applicationReference,
+    claimReference: reference,
+    notifyTemplateId: templateId,
+    emailAddress: email,
+    customParams: personalisation,
+    dateTime: new Date().toISOString()
+  }
 
-    if (validateSFDClaim(sfdMessage)) {
-        return await sendMessage(sfdMessage, sfdRequestMsgType, sfdMessageQueue)
-    } else {
-        return sendMessage({ applicationState: states.failed }, sfdRequestMsgType, sfdMessageQueue, { templateId })
-    }
+  if (validateSFDClaim(sfdMessage)) {
+    return await sendMessage(sfdMessage, sfdRequestMsgType, sfdMessageQueue)
+  } else {
+    return sendMessage({ applicationState: states.failed }, sfdRequestMsgType, sfdMessageQueue, { templateId })
+  }
 }
 
 module.exports = sendSFDEmail
--- app/messaging/schema/submit-sfd-schema.js
+++ app/messaging/schema/submit-sfd-schema.js
@@ -1,8 +1,8 @@
 const joi = require('joi')
 const util = require('util')
 
-const nineDigitId = joi.string().pattern(/^\d{9}$/);
-const tenDigitId = joi.string().pattern(/^\d{10}$/);
+const nineDigitId = joi.string().pattern(/^\d{9}$/)
+const tenDigitId = joi.string().pattern(/^\d{10}$/)
 const email = joi
   .string()
   .pattern(/^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/)
@@ -10,24 +10,24 @@ const email = joi
   .max(320)
 
 const submitSFDSchema = joi.object({
-    crn: tenDigitId.required(),
-    sbi: nineDigitId.required(),
-    agreementReference: joi.string().required(),
-    claimReference: joi.string().max(14),
-    notifyTemplateId: joi.string().guid({ version: 'uuidv4' }).required(),
-    emailAddress: email.required(),
-    customParams: joi.object().required(),
-    dateTime: joi.date().required()
+  crn: tenDigitId.required(),
+  sbi: nineDigitId.required(),
+  agreementReference: joi.string().required(),
+  claimReference: joi.string().max(14),
+  notifyTemplateId: joi.string().guid({ version: 'uuidv4' }).required(),
+  emailAddress: email.required(),
+  customParams: joi.object().required(),
+  dateTime: joi.date().required()
 })
 
 const validateSFDClaim = (event) => {
-    const validate = submitSFDSchema.validate(event)
+  const validate = submitSFDSchema.validate(event)
 
-    if (validate.error) {
-        console.log('Submit claim validation error:', util.inspect(validate.error, false, null, true))
-        return false
-    }
-    return true
+  if (validate.error) {
+    console.log('Submit claim validation error:', util.inspect(validate.error, false, null, true))
+    return false
+  }
+  return true
 }
 
 module.exports = validateSFDClaim
