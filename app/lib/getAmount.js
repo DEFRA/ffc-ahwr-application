7c94f4405d7cb74736c3f4c17cf9aef4b4cb1c68
Rob Catton <robmcatton@gmail.com>
2025-01-08 14:04:57 +0000

    Removed price config

--- app/lib/getAmount.js
+++ app/lib/getAmount.js
@@ -2,57 +2,6 @@ import { getBlob } from '../storage/getBlob.js'
 import { config } from '../config/index.js'
 import { livestockTypes, claimType, testResults, piHunt as piHuntMap, piHuntAllAnimals as piHuntAllAnimalsMap } from './../constants/index.js'
 
-const claimPricesConfig = {
-  review: {
-    beef: {
-      value: 522,
-      code: 'AHWR-Beef'
-    },
-    dairy: {
-      value: 372,
-      code: 'AHWR-Dairy'
-    },
-    pigs: {
-      value: 557,
-      code: 'AHWR-Pigs'
-    },
-    sheep: {
-      value: 436,
-      code: 'AHWR-Sheep'
-    }
-  },
-  followUp: {
-    beef: {
-      value: {
-        positive: 837,
-        negative: {
-          noPiHunt: 215,
-          yesPiHunt: 837
-        }
-      },
-      code: 'AHWR-Beef'
-    },
-    dairy: {
-      value: {
-        positive: 1714,
-        negative: {
-          noPiHunt: 215,
-          yesPiHunt: 1714
-        }
-      },
-      code: 'AHWR-Dairy'
-    },
-    pigs: {
-      value: 923,
-      code: 'AHWR-Pigs'
-    },
-    sheep: {
-      value: 639,
-      code: 'AHWR-Sheep'
-    }
-  }
-}
-
 const getPiHuntValue = (reviewTestResults, piHunt, piHuntAllAnimals, pricesConfig, claimType, typeOfLivestock) => {
   const optionalPiHuntValue = (piHunt === piHuntMap.yes && piHuntAllAnimals === piHuntAllAnimalsMap.yes) ? 'yesPiHunt' : 'noPiHunt'
 
@@ -85,12 +34,7 @@ export const getAmount = async (payload) => {
   const { type, data } = payload
   const typeOfClaim = type === claimType.review ? 'review' : 'followUp'
 
-  let pricesConfig
-  if (process.env.NODE_ENV === 'development') {
-    pricesConfig = claimPricesConfig 
-  } else {
-    pricesConfig = await getBlob('claim-prices-config.json')
-  }
+  const pricesConfig = await getBlob('claim-prices-config.json')
   
   const { typeOfLivestock } = data
 
