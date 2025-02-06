bd32bd662b2a8d021ab8975d5aab9c95f3a79660
Matthew O'Donoghue <mattyod@gmail.com>
2025-02-06 13:58:04 +0000

    improvement(AHWR-29): return empty history array (#299)
    No longer returning null if there are no history events, so the
    back-office doesn't need to convert them back to an array.

--- test/integration/narrow/routes/api/application-history.test.js
+++ test/integration/narrow/routes/api/application-history.test.js
@@ -6,45 +6,35 @@ jest.mock('../../../../../app/azure-storage/application-status-repository')
 describe('Application history test', () => {
   beforeEach(async () => {
     jest.clearAllMocks()
-    await server.start()
   })
 
-  afterEach(async () => {
-    await server.stop()
-  })
-  const reference = 'ABC-1234'
-  const url = `/api/application/history/${reference}`
-
-  describe(`GET ${url} route`, () => {
-    test('returns 200 with data', async () => {
-      getApplicationHistory.mockResolvedValue(
-        [{ ChangedOn: '2023-03-23T10:00:12.000Z', Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 91\n}', ChangedBy: 'Daniel Jones' },
-          { ChangedOn: '2023-03-24T09:30:00.000Z', Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 2\n}', ChangedBy: 'Daniel Jones' },
-          { ChangedOn: '2023-03-25T11:10:15:12.000Z', Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 10\n}', ChangedBy: 'Amanda Hassan' }]
-      )
-
-      const options = {
-        method: 'GET',
-        url: '/api/application/history/ABC-1234'
+  test('returns historyRecords', async () => {
+    const historyRecords = [
+      {
+        ChangedOn: '2023-03-23T10:00:12.000Z',
+        Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 91\n}',
+        ChangedBy: 'Daniel Jones'
+      },
+      {
+        ChangedOn: '2023-03-24T09:30:00.000Z',
+        Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 2\n}',
+        ChangedBy: 'Daniel Jones'
+      },
+      {
+        ChangedOn: '2023-03-25T11:10:15:12.000Z',
+        Payload: '{\n  "reference": "AHWR-7C72-8871",\n  "statusId": 10\n}',
+        ChangedBy: 'Amanda Hassan'
       }
-      const res = await server.inject(options)
-      expect(res.statusCode).toBe(200)
-      const $ = JSON.parse(res.payload)
-      expect($.historyRecords).not.toBeNull()
-      expect($.historyRecords.length).toBe(3)
-    })
+    ]
 
-    test('returns 200 with no data', async () => {
-      getApplicationHistory.mockResolvedValue(null)
+    getApplicationHistory.mockResolvedValue(historyRecords)
 
-      const options = {
-        method: 'GET',
-        url: '/api/application/history/ABC-1234'
-      }
-      const res = await server.inject(options)
-      expect(res.statusCode).toBe(200)
-      const $ = JSON.parse(res.payload)
-      expect($.historyRecords).toBeNull()
-    })
+    const options = {
+      method: 'GET',
+      url: '/api/application/history/AHWR-7C72-8871'
+    }
+    const res = await server.inject(options)
+
+    expect(JSON.parse(res.payload)).toEqual({ historyRecords })
   })
 })
