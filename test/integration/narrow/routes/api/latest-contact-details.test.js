063d86f7698ed5ea1e216db80ca3fbd021c4eeea
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-02-20 13:42:42 +0000

    feat(AHWR-546): increased version and fixed lint

--- test/integration/narrow/routes/api/latest-contact-details.test.js
+++ test/integration/narrow/routes/api/latest-contact-details.test.js
@@ -4,7 +4,6 @@ import { getApplication } from '../../../../../app/repositories/application-repo
 jest.mock('../../../../../app/repositories/application-repository')
 
 describe('latest-contact-details', () => {
-
   beforeEach(async () => {
     jest.clearAllMocks()
     await server.start()
@@ -15,28 +14,27 @@ describe('latest-contact-details', () => {
   })
 
   describe('GET /api/application/latest-contact-details', () => {
-
     test('should return contact details when reference matches an application with contact details', async () => {
       getApplication.mockResolvedValueOnce({
         dataValues: {
-          reference: "TEMP-MFV1-AAAA",
+          reference: 'TEMP-MFV1-AAAA',
           createdBy: 'admin',
-          createdAt: new Date("2025-01-01"),
+          createdAt: new Date('2025-01-01'),
           data: {
-            type: "EE",
-            reference: "TEMP-MFV1-AAAA",
+            type: 'EE',
+            reference: 'TEMP-MFV1-AAAA',
             declaration: true,
-            offerStatus: "accepted",
+            offerStatus: 'accepted',
             organisation: {
-              "sbi": "106642000",
-              "name": "Willow Farm",
-              "email": "john.doe@gmail.com",
-              "address": "Sunnybrook Farm, 123 Harvest Lane, Meadowville, Oxfordshire, OX12 3AB, UK",
-              "orgEmail": "willowfarm@gmail.com",
-              "userType": "newUser",
-              "farmerName": "John Jim Doe"
+              sbi: '106642000',
+              name: 'Willow Farm',
+              email: 'john.doe@gmail.com',
+              address: 'Sunnybrook Farm, 123 Harvest Lane, Meadowville, Oxfordshire, OX12 3AB, UK',
+              orgEmail: 'willowfarm@gmail.com',
+              userType: 'newUser',
+              farmerName: 'John Jim Doe'
             },
-            confirmCheckDetails: "yes"
+            confirmCheckDetails: 'yes'
           }
         }
       })
@@ -49,30 +47,30 @@ describe('latest-contact-details', () => {
 
       expect(res.statusCode).toBe(200)
       expect(JSON.parse(res.payload)).toEqual({
-        name: "Willow Farm",
-        orgEmail: "willowfarm@gmail.com",
-        farmerName: "John Jim Doe",
-        email: "john.doe@gmail.com"
+        name: 'Willow Farm',
+        orgEmail: 'willowfarm@gmail.com',
+        farmerName: 'John Jim Doe',
+        email: 'john.doe@gmail.com'
       })
     })
 
     test('should return empty contact details when reference matches an application without contact details', async () => {
       getApplication.mockResolvedValueOnce({
         dataValues: {
-          reference: "TEMP-MFV1-AAAA",
+          reference: 'TEMP-MFV1-AAAA',
           createdBy: 'admin',
-          createdAt: new Date("2025-01-01"),
+          createdAt: new Date('2025-01-01'),
           data: {
-            type: "EE",
-            reference: "TEMP-MFV1-AAAA",
+            type: 'EE',
+            reference: 'TEMP-MFV1-AAAA',
             declaration: true,
-            offerStatus: "accepted",
+            offerStatus: 'accepted',
             organisation: {
-              "sbi": "106642000",
-              "address": "Sunnybrook Farm, 123 Harvest Lane, Meadowville, Oxfordshire, OX12 3AB, UK",
-              "userType": "newUser",
+              sbi: '106642000',
+              address: 'Sunnybrook Farm, 123 Harvest Lane, Meadowville, Oxfordshire, OX12 3AB, UK',
+              userType: 'newUser'
             },
-            confirmCheckDetails: "yes"
+            confirmCheckDetails: 'yes'
           }
         }
       })
