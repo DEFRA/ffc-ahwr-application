23075d00067c046314a82b4cbb02899b8f33e36f
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-04-25 08:40:45 +0100

    fix(AHWR-681): Send out events when data updated from back office

--- test/unit/app/repository/claim-repository.test.js
+++ test/unit/app/repository/claim-repository.test.js
@@ -7,11 +7,14 @@ import {
   isURNNumberUnique,
   searchClaims,
   setClaim,
-  updateClaimByReference
+  updateClaimByReference,
+  updateClaimData
 } from '../../../../app/repositories/claim-repository'
 import { buildData } from '../../../../app/data'
 import { livestockTypes } from '../../../../app/constants'
-import { Op } from 'sequelize'
+import { Op, Sequelize } from 'sequelize'
+import { claimDataUpdateEvent } from '../../../../app/event-publisher/claim-data-update-event.js'
+import { findApplication } from '../../../../app/repositories/application-repository.js'
 
 jest.mock('../../../../app/data', () => {
   return {
@@ -28,7 +31,8 @@ jest.mock('../../../../app/data', () => {
           findAll: jest.fn()
         },
         claim_update_history: {
-          findAll: jest.fn()
+          findAll: jest.fn(),
+          create: jest.fn()
         },
         status: 'mock-status'
       }
@@ -36,7 +40,8 @@ jest.mock('../../../../app/data', () => {
   }
 })
 
-jest.mock('../../../../app/repositories/application-repository', () => {})
+jest.mock('../../../../app/repositories/application-repository')
+jest.mock('../../../../app/event-publisher/claim-data-update-event')
 
 const MOCK_SEND_EVENTS = jest.fn()
 
@@ -613,6 +618,66 @@ describe('Claim repository test', () => {
       }, note, logger)).rejects.toBe('Something failed')
     })
   })
+
+  describe('updateClaimData', () => {
+    test('should update claim data successfully', async () => {
+      buildData.models.claim.update.mockResolvedValueOnce([
+        1,
+        [
+          {
+            dataValues: {
+              applicationReference: 'REF-UPDATE',
+              statusId: 2,
+              updatedAt: new Date(),
+              updatedBy: 'admin'
+            }
+          }
+        ]
+      ])
+      findApplication.mockResolvedValueOnce({
+        data: {
+          organisation: {
+            sbi: '123456789',
+            email: 'business@email.com'
+          }
+        }
+      })
+      MOCK_SEND_EVENTS.mockResolvedValueOnce(null)
+
+      await updateClaimData('REF-UPDATE', 'vetsName', 'Geoff', 'Bill', 'note here', 'Admin')
+
+      expect(buildData.models.claim.update).toHaveBeenCalledWith({
+        data: Sequelize.fn(
+          'jsonb_set',
+          Sequelize.col('data'),
+          Sequelize.literal('\'{vetsName}\''),
+          Sequelize.literal('\'"Geoff"\'')
+        )
+      }, {
+        where: { reference: 'REF-UPDATE' },
+        returning: true
+      })
+      expect(buildData.models.claim_update_history.create).toHaveBeenCalledWith({
+        applicationReference: 'REF-UPDATE',
+        reference: 'REF-UPDATE',
+        createdBy: 'Admin',
+        eventType: 'claim-vetsName',
+        updatedProperty: 'vetsName',
+        oldValue: 'Bill',
+        newValue: 'Geoff',
+        note: 'note here'
+      })
+      expect(claimDataUpdateEvent).toHaveBeenCalledWith({
+        applicationReference: 'REF-UPDATE',
+        reference: 'REF-UPDATE',
+        updatedProperty: 'vetsName',
+        oldValue: 'Bill',
+        newValue: 'Geoff',
+        note: 'note here'
+      }, 'claim-vetName', 'Admin', expect.any(Date), '123456789')
+    })
+  })
+
   describe('Search Claim', () => {
     test.each([
       { searchText: 'AHWR-7C72-8871', searchType: 'ref', sort: { field: 'claim number', direction: undefined } },
