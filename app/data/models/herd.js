c26d7b58182045399b9c6bd3d8e6d73beffe0e3f
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-04-28 11:06:46 +0100

    feat(AHWR-662): herds database table and model

---
+++ app/data/models/herd.js
@@ -0,0 +1,53 @@
+export const herd = (sequelize, DataTypes) => {
+  const herdModel = sequelize.define(
+    'herd',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: sequelize.UUIDV4
+      },
+      version: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      applicationReference: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(val) {
+          this.setDataValue('applicationReference', val.toUpperCase())
+        }
+      },
+      herdName: DataTypes.STRING,
+      cph: DataTypes.STRING,
+      othersOnSameCph: DataTypes.BOOLEAN,
+      herdReasons: DataTypes.STRING,
+      isCurrent: DataTypes.BOOLEAN,
+      createdBy: DataTypes.STRING,
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedBy: DataTypes.STRING,
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    },
+    {
+      freezeTableName: true,
+      tableName: 'herd',
+      updatedAt: false
+    }
+  )
+
+  herdModel.associate = function (models) {
+    herdModel.belongsTo(models.application, {
+      foreignKey: 'applicationReference',
+      targetKey: 'reference',
+      as: 'application'
+    })
+  }
+
+  return herdModel
+}
