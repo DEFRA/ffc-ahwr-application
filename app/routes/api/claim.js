2bd7eea6ecee2d86d6804120da1d46fe9d5fdb77
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-05-16 11:43:32 +0100

    removed log

--- app/routes/api/claim.js
+++ app/routes/api/claim.js
@@ -355,7 +355,6 @@ export const claimHandlers = [
         const { statusId } = await requiresComplianceCheck('claim')
         const { herd, ...payloadData } = payload.data
 
-        console.log({herd})
         let claim
 
         await sequelize.transaction(async () => {
