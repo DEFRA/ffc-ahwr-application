8573839d76b210c996dfcf350bb79d7068405d73
Rob Catton <robmcatton@gmail.com>
2025-05-27 10:10:55 +0100

    Increasing test coverae

--- test/unit/app/repository/claim-repository.test.js
+++ test/unit/app/repository/claim-repository.test.js
@@ -209,6 +209,48 @@ describe('Claim repository: Search Claim', () => {
     })
   })
 
+  test('it applies a default sort if there is no field providedd', async () => {
+    const search = { type: 'ref', text: 'REBC-1212-FSFW' }
+    const filter = { field: 'X', op: 'lte', value: 'Z' }
+    const offset = 10
+    const limit = 20
+    const sort = {}
+
+    const result = await searchClaims(search, filter, offset, limit, sort)
+
+    expect(buildData.models.claim.findAll).toHaveBeenCalledWith({
+      include: [
+        { attributes: ['status'], model: 'mock-status' },
+        { attributes: ['data'], model: { findAll: expect.anything() } },
+        {
+          as: 'flags',
+          attributes: ['appliesToMh'],
+          model: 'mock-flag',
+          required: false,
+          where: { deletedBy: null }
+        }
+      ],
+      limit,
+      offset,
+      order: [['createdAt', 'ASC']],
+      where: { [filter.field]: expect.any(Object), reference: search.text }
+    })
+
+    expect(result).toEqual({
+      claims: [
+        {
+          data: { herdId: 'aaa111', herdVersion: 1 },
+          herd: { id: 'aaa111', version: 1, herdName: 'My first herd' }
+        },
+        {
+          data: { herdId: 'aaa222', herdVersion: 1 },
+          herd: { id: 'aaa222', version: 1, herdName: 'My second herd' }
+        }
+      ],
+      total: 2
+    })
+  })
+
   test('it applies the search for species if provided', async () => {
     const search = { type: 'species', text: 'sheep' }
     const filter = { field: 'X', op: 'lte', value: 'Z' }
