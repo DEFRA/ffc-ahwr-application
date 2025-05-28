97e722b16b84731538a5a8ee7ef7d5c8ff2c727f
RobCatton <41433657+RobCatton@users.noreply.github.com>
2025-05-28 17:12:03 +0100

    feat(AHWR-789): Added herd info to claim API (#338)

--- test/unit/app/repository/herd-repository.test.js
+++ test/unit/app/repository/herd-repository.test.js
@@ -152,5 +152,40 @@ describe('herdService', () => {
       expect(buildData.models.herd.findAll).toHaveBeenCalledWith({ where: { applicationReference: 'AHWR-0AD3-3322', species: 'sheep', isCurrent: true } })
       expect(result).toEqual(mockResult)
     })
+
+    it('should query herds with no species if no species provided', async () => {
+      const mockResult = [
+        {
+          dataValues: {
+            id: '0f5d4a26-6a25-4f5b-882e-e18587ba9f4b',
+            createdAt: '2024-02-14T09:59:46.756Z',
+            applicationReference: 'AHWR-0AD3-3322',
+            version: 1,
+            herdName: 'Sample herd one',
+            cph: '43200',
+            herdReasons: ['separateManagementNeeds'],
+            createdBy: 'admin'
+          }
+        },
+        {
+          dataValues: {
+            id: '1a2d4a26-6a25-4f5b-882e-e18587ba9f4b',
+            createdAt: '2024-02-14T09:59:46.756Z',
+            applicationReference: 'AHWR-0AD3-3322',
+            version: 1,
+            herdName: 'Sample herd two',
+            cph: '12321',
+            herdReasons: ['separateManagementNeeds'],
+            createdBy: 'admin'
+          }
+        }
+      ]
+      models.herd.findAll.mockResolvedValueOnce(mockResult)
+
+      const result = await getHerdsByAppRefAndSpecies('AHWR-0AD3-3322')
+
+      expect(buildData.models.herd.findAll).toHaveBeenCalledWith({ where: { applicationReference: 'AHWR-0AD3-3322', isCurrent: true } })
+      expect(result).toEqual(mockResult)
+    })
   })
 })
