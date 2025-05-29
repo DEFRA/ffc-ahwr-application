7444f8b556135985aa3c7aa5f0dcc0387f9ee282
Ben Hope <bendef24@gmail.com>
2025-05-29 08:44:50 +0100

    feat(AHWR-816): support users rejecting multiple herd T&Cs, add flags to getLatestApplicationsBySbi

--- app/lib/context-helper.js
+++ app/lib/context-helper.js
@@ -15,10 +15,6 @@ export const isMultipleHerdsUserJourney = (dateOfVisit, agreementFlags) => {
     return false
   }
 
-  // only check for rejected T&Cs flag if MH enabled and visit date on/after golive
-  if (agreementFlags?.some(f => f.appliesToMh)) {
-    return false
-  }
-
-  return true
+  // check for rejected T&Cs flag, if absent then is multiple herds journey
+  return !agreementFlags?.some(f => f.appliesToMh)
 }
