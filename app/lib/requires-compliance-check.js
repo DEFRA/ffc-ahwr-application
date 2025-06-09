1d5ec58fa68cafbbfc2f5a11c1a87ceaaa852c49
Andrew Timney <andrew.timney@gmail.com>
2025-06-09 14:35:16 +0100

    fix: requires compliance check tests, remove old work implementation

--- app/lib/requires-compliance-check.js
+++ app/lib/requires-compliance-check.js
@@ -1,30 +1,20 @@
 import { config } from '../config/index.js'
 import { applicationStatus } from '../constants/index.js'
 import { getAllClaimedClaims } from '../repositories/claim-repository.js'
-import { getAllClaimedApplications } from '../repositories/application-repository.js'
 
-export const requiresComplianceCheck = async (claimOrApplication) => {
+export const requiresComplianceCheck = async () => {
   const claimStatusIds = [applicationStatus.inCheck, applicationStatus.readyToPay, applicationStatus.rejected, applicationStatus.onHold, applicationStatus.recommendToPay, applicationStatus.recommendToReject]
-  let complianceCheckRatio
 
-  let claimedApplicationsCount
-
-  if (claimOrApplication === 'claim') {
-    claimedApplicationsCount = await getAllClaimedClaims(claimStatusIds)
-    complianceCheckRatio = Number(config.compliance.endemicsComplianceCheckRatio)
-  } else {
-    claimedApplicationsCount = await getAllClaimedApplications(claimStatusIds)
-    complianceCheckRatio = Number(config.compliance.complianceCheckRatio)
-  }
+  const claimedApplicationsCount = await getAllClaimedClaims(claimStatusIds)
+  const complianceCheckRatio = Number(config.complianceCheckRatio)
 
   let statusId = applicationStatus.inCheck
-  let claimed = false
 
   if (complianceCheckRatio <= 0 || (claimedApplicationsCount + 1) % complianceCheckRatio !== 0) {
     // if the claim does not trigger the configurable compliance check volume ratio, set as onHold
     // if complianceCheckRatio is 0 or less this means compliance checks are turned off
     statusId = applicationStatus.onHold
-    claimed = true
   }
-  return { claimed, statusId }
+
+  return statusId
 }
