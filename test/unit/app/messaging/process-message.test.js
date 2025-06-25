ba34cc5b4fe186d4d9ebfe75049f76134233bf34
Rob Catton <robmcatton@gmail.com>
2025-06-25 19:56:25 +0100

    Further test improvements

--- test/unit/app/messaging/process-message.test.js
+++ test/unit/app/messaging/process-message.test.js
@@ -1,12 +1,13 @@
 import { config } from '../../../../app/config'
 import { processApplicationMessage } from '../../../../app/messaging/process-message'
 import { processApplicationQueue } from '../../../../app/messaging/application/process-application'
+import { setPaymentStatusToPaid } from '../../../../app/messaging/application/set-payment-status-to-paid'
 
 jest.mock('../../../../app/messaging/application/process-application')
 jest.mock('applicationinsights', () => ({ defaultClient: { trackException: jest.fn(), trackEvent: jest.fn() }, dispose: jest.fn() }))
-jest.mock('../../../../app/messaging/application/process-application')
+jest.mock('../../../../app/messaging/application/set-payment-status-to-paid')
 
-const { applicationRequestMsgType } = config
+const { applicationRequestMsgType, moveClaimToPaidMsgType } = config
 
 describe('Process Message test', () => {
   const sessionId = '8e5b5789-dad5-4f16-b4dc-bf6db90ce090'
@@ -40,6 +41,24 @@ describe('Process Message test', () => {
 
     await processApplicationMessage(message, receiver)
     expect(processApplicationQueue).toHaveBeenCalledTimes(1)
+    expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
+  })
+
+  test(`${moveClaimToPaidMsgType} message calls setPaymentStatusToPaid`, async () => {
+    const message = {
+      messageId: '1234567890',
+      body: {
+        claimRef: 'FUBC-1234-5678',
+        sbi: '123456789'
+      },
+      applicationProperties: {
+        type: moveClaimToPaidMsgType
+      },
+      sessionId
+    }
+
+    await processApplicationMessage(message, receiver)
+    expect(setPaymentStatusToPaid).toHaveBeenCalledTimes(1)
     expect(receiver.completeMessage).toHaveBeenCalledTimes(1)
   })
 })
