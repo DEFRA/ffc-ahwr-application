6def9e2737cdc24249c563d5616b78129986ef7d
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-08-18 16:14:09 +0100

    fix sonar issue

--- app/repositories/claim-repository.js
+++ app/repositories/claim-repository.js
@@ -1,10 +1,10 @@
 import { REDACT_PII_VALUES, APPLICATION_REFERENCE_PREFIX_OLD_WORLD } from 'ffc-ahwr-common-library'
 import { buildData } from '../data/index.js'
 import { raiseClaimEvents, raiseHerdEvent } from '../event-publisher/index.js'
-import { startandEndDate } from '../lib/date-utils.js'
 import { claimDataUpdateEvent } from '../event-publisher/claim-data-update-event.js'
 import { Op, QueryTypes, Sequelize } from 'sequelize'
 import { findApplication } from './application-repository.js'
+import { applySearchConditions } from './claim/search.js'
 
 const { models, sequelize } = buildData
 
@@ -157,82 +157,6 @@ const evalSortField = (sort) => {
   }
 
   return orderBySortField[field] || ['createdAt', direction]
-}
-
-const applySearchConditions = (query, search) => {
-  if (!search?.text || !search?.type) {
-    return
-  }
-
-  const { text, type } = search
-
-  switch (type) {
-    case 'ref':
-      query.where = { reference: text }
-      break
-    case 'appRef':
-      query.where = { applicationReference: text }
-      break
-    case 'type':
-      query.where = { type: text }
-      break
-    case 'species':
-      query.where = { 'data.typeOfLivestock': text }
-      break
-    case 'status':
-      query.include = [
-        {
-          model: models.application,
-          attributes: ['data']
-        },
-        {
-          model: models.status,
-          attributes: ['status'],
-          where: { status: { [Op.iLike]: `%${text}%` } }
-        },
-        {
-          model: models.flag,
-          as: 'flags',
-          attributes: ['appliesToMh'],
-          where: { deletedBy: null },
-          required: false
-        }
-      ]
-      break
-    case 'sbi': {
-      query.include = [
-        {
-          model: models.status,
-          attributes: ['status']
-        },
-        {
-          model: models.application,
-          attributes: ['data'],
-          where: { 'data.organisation.sbi': text }
-        },
-        {
-          model: models.flag,
-          as: 'flags',
-          attributes: ['appliesToMh'],
-          where: { deletedBy: null },
-          required: false
-        }
-      ]
-      break
-    }
-    case 'date': {
-      const { startDate, endDate } = startandEndDate(text)
-      query.where = {
-        createdAt: {
-          [Op.gte]: startDate,
-          [Op.lt]: endDate
-        }
-      }
-      break
-    }
-    default:
-      break
-  }
 }
 
 export const searchClaims = async (search, filter, offset, limit, sort = { field: 'createdAt', direction: 'DESC' }) => {
